ea997d4ae84de0e8baf5499d7f6da92a75ac354a
Cassio M. M. Pereira <cassiomartini@gmail.com>
2014-02-12 10:43:19 -0200

    license info

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a Java implementation of [Conway's game of life](http://en.wikipedia.org/wiki/Conway's_Game_of_Life).
+
+To run the program you will need [Maven](http://maven.apache.org/) installed.
+
+At the project directory run:
+
+`mvn install`
+
+`mvn exec:java -DmainClass="br.cassio.life.GameOfLife"`
--- src/main/java/br/cassio/life/GameOfLife.java
+++ src/main/java/br/cassio/life/GameOfLife.java
@@ -1,3 +1,19 @@
+/*  Copyright (C) 2014  Cássio M. M. Pereira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package br.cassio.life;
 
 import java.awt.BorderLayout;
@@ -15,7 +31,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
- * Hello world!
+ * Implementation of Conway's Game of Life
+ * 
+ * @author Cássio M. M. Pereira <cassiomartini@gmail.com>
  *
  */
 public class GameOfLife extends JFrame implements ActionListener
